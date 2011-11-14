2bb37c4ed4a88cb2efc73e58468afbe039e2c7df
donabrams <donabrams@gmail.com>
2011-11-14 18:27:13 -0500

    add an empty rails app for now, next move the require static files to the right place

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Harbor::Application.config.secret_token = '550264850581e28ae69d4a51e67cd69ca4558a1c8d5eef177dfbc4ec7246a6076f3198cb0ac4451c1509366d52785f8434805bf2c2ca99b8ec010b64486ba7e9'
